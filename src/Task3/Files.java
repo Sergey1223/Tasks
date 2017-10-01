548ac31a2647d16bf215e4ae531e12c1b76ce28a
Sergey Mytko <ps2.scph@gmail.com>
2017-10-01 21:34:42 +0400

    added package Task3

---
+++ src/Task3/Files.java
@@ -0,0 +1,30 @@
+package Task3;
+
+import java.io.*;
+
+public class Files {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = null;
+        Writer writer = null;
+        try {
+            bufferedReader = new BufferedReader(new FileReader("out\\production\\files\\input.txt"));
+            String[] numbers = bufferedReader.readLine().split("\\ ");
+            long result = Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+            System.out.println(result);
+            System.out.println(Long.toString(result));
+            writer = new FileWriter("out\\production\\files\\output.txt");
+            writer.append(Long.toString(result));
+
+        }
+        catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+        finally {
+            bufferedReader.close();
+            writer.close();
+        }
+
+    }
+}
